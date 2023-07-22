fafa788f543c832da59229808cb71baa3634b023
WilsonWuux1 <wpenate@isbelasoft.com>
2023-07-22 15:24:03 -0600

    Ejercicio 2

--- index.py
+++ index.py
@@ -4,24 +4,33 @@ nombre_archivo = "archivo.txt"
 ruta_archivo = "C:/Users/Wuux/Desktop/Compiladores"  # Opcional: especifica la ruta del archivo
 ruta_completa = os.path.join(ruta_archivo, nombre_archivo)
 
-
+# Integrantes Wilson Peñate y Eddison Rafael
 
 if os.path.exists(ruta_completa):
-    # Leer el archivo para contar palabras y líneas
+    # Ejercicio 1
     archivo = open(ruta_completa, "r")
     contenido = archivo.read()
     archivo.close()
-
-    # Contar cantidad de líneas
+    
     num_lineas = len(contenido.split('\n'))
 
-    # Contar cantidad de palabras
     num_palabras = len(contenido.split())
 
     print("Conteo de palabras:", num_palabras)
     print("Conteo de líneas:", num_lineas)
 
-    print(contenido)
+    # Ejercicio 2
+    numero_ingresado = input("Ingresa un número: ")
+
+    try:
+        numero = float(numero_ingresado)
+        if numero.is_integer():
+            print("entero")
+        else:
+            print("decimal")
+    except ValueError:
+        print("Error")
+        
 else:
     archivo = open(ruta_completa, "w")
     archivo.write("Curso de compiladores.\n")
